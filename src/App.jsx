6fac06c49cf7fd1db64c6a17e277885e9d4a21a5
Jake <jake@jakec.dev>
2022-06-23 08:09:01 +1000

    Replace manually imported actions in state context component with a useActions custom hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import { FaSync } from "react-icons/fa";
 import AddItem from "./components/AddItem";
+import { AppStateProvider } from "./contexts/appState";
 import GearTable from "./components/GearTable";
 import Header from "./components/Header";
 import ItemInspector from "./components/ItemInspector";
-import { AppStateProvider } from "./contexts/appState";
 import "@csstools/normalize.css";
 import "./App.css";
 
@@ -12,49 +12,33 @@ import "./App.css";
 // const serverURL = "https://jakecdev-travel-planner-server.herokuapp.com";
 
 function App() {
-  /* const [items, setItems] = useState([]);
-  const [selectedItems, setSelectedItems] = useState([]);
+  // const deleteItem = async (id) => {
+  //   const data = await fetch(`${serverURL}/item/${id}`, {
+  //     method: "DELETE",
+  //   }).then((resp) => resp.json());
+  // };
 
-  const deleteItem = async (id) => {
-    const data = await fetch(`${serverURL}/item/${id}`, {
-      method: "DELETE",
-    }).then((resp) => resp.json());
-    console.log(data);
-    setItems(items.filter((item) => item.id !== id));
-    setSelectedItems(selectedItems.filter((o) => o.id !== id));
-  };
+  // const updateItem = async (modifiedItem) => {
+  //   const data = await fetch(`${serverURL}/item`, {
+  //     method: "PUT",
+  //     headers: { "Content-Type": "application/json" },
+  //     body: JSON.stringify(modifiedItem),
+  //   }).then((resp) => resp.json());
+  // };
 
-  const updateItem = async (modifiedItem) => {
-    const data = await fetch(`${serverURL}/item`, {
-      method: "PUT",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(modifiedItem),
-    }).then((resp) => resp.json());
-    console.log(data);
-    setItems(
-      items.map((item) => {
-        if (item.id === modifiedItem.id) return modifiedItem;
-        return item;
-      })
-    );
-    setSelectedItems([modifiedItem]);
-  };
+  // const toggleItemSelect = async (item) => {
+  //   const data = await fetch(`${serverURL}/item/${item.id}`).then((resp) =>
+  //     resp.json()
+  //   );
+  // };
 
-  const toggleItemSelect = async (item) => {
-    // For testing purposes only: GET an item by ID from server
-    const data = await fetch(`${serverURL}/item/${item.id}`).then((resp) =>
-      resp.json()
-    );
-  };
+  // const syncFromServer = async () => {
+  //   const data = await fetch(`${serverURL}/items`).then((resp) => resp.json());
+  // };
 
-  const syncFromServer = async () => {
-    const data = await fetch(`${serverURL}/items`).then((resp) => resp.json());
-    setItems(data);
-  };
-
-  useEffect(() => {
-    syncFromServer();
-  }, []); */
+  // useEffect(() => {
+  //   syncFromServer();
+  // }, []);
 
   return (
     <AppStateProvider>
--- src/contexts/appState.jsx
+++ src/contexts/appState.jsx
@@ -1,17 +1,15 @@
 import React, { createContext, useContext, useReducer } from "react";
 import PropTypes from "prop-types";
 import { reducer, initialState } from "../state/reducers";
-import itemsActions from "../state/actions/itemsActions";
-import selectionActions from "../state/actions/selectionActions";
+import useActions from "../state/actions";
 
 const Context = createContext();
 
 function AppStateProvider({ children }) {
   const [state, dispatch] = useReducer(reducer, initialState);
-  const actions = {
-    itemsActions: itemsActions(dispatch),
-    selectionActions: selectionActions(dispatch),
-  };
+  const actions = useActions(dispatch);
+  // const value = useMemo(() => ({ ...state, ...actions }), []);
+  // return <Context.Provider value={value}>{children}</Context.Provider>;
   return (
     // eslint-disable-next-line react/jsx-no-constructed-context-values
     <Context.Provider value={{ ...state, ...actions }}>
