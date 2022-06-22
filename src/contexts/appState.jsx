83ca0f6df750e3776861b80c267fd302650aaae8
Jake <jake@jakec.dev>
2022-06-23 08:26:28 +1000

    Memoize state and actions object that is passed into the state context provider's value prop

--- src/contexts/appState.jsx
+++ src/contexts/appState.jsx
@@ -1,4 +1,4 @@
-import React, { createContext, useContext, useReducer } from "react";
+import React, { createContext, useContext, useMemo, useReducer } from "react";
 import PropTypes from "prop-types";
 import { reducer, initialState } from "../state/reducers";
 import useActions from "../state/actions";
@@ -8,14 +8,8 @@ const Context = createContext();
 function AppStateProvider({ children }) {
   const [state, dispatch] = useReducer(reducer, initialState);
   const actions = useActions(dispatch);
-  // const value = useMemo(() => ({ ...state, ...actions }), []);
-  // return <Context.Provider value={value}>{children}</Context.Provider>;
-  return (
-    // eslint-disable-next-line react/jsx-no-constructed-context-values
-    <Context.Provider value={{ ...state, ...actions }}>
-      {children}
-    </Context.Provider>
-  );
+  const value = useMemo(() => ({ ...state, ...actions }), [state]);
+  return <Context.Provider value={value}>{children}</Context.Provider>;
 }
 
 AppStateProvider.propTypes = {
