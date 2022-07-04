617db7881fa6df833ee472e9bee1ad741ff61782
Jake C <git@jakec.dev>
2022-07-04 16:46:35 +1000

    Use jest-fetch-mock package

--- src/api/itemsAPI.test.js
+++ src/api/itemsAPI.test.js
@@ -1,4 +1,3 @@
-import { jest } from "@jest/globals";
 import {
   createItem,
   deleteItem,
@@ -13,22 +12,15 @@ const items = [
   { id: 3, name: "Toothpaste", brand: "Colgate" },
 ];
 
-afterEach(() => {
-  fetch.mockClear();
+beforeEach(() => {
+  fetch.resetMocks();
 });
 
 describe("createItem()", () => {
   it("should return the new item", async () => {
     const newItem = { id: 4, name: "test name" };
-    global.fetch = jest.fn(() =>
-      Promise.resolve({
-        json: () =>
-          Promise.resolve({
-            status: "success",
-            message: "it worked",
-            data: newItem,
-          }),
-      })
+    fetch.mockResponseOnce(
+      JSON.stringify({ status: "success", data: newItem })
     );
     const resp = await createItem(newItem);
     expect(resp).toMatchObject(newItem);
@@ -37,14 +29,11 @@ describe("createItem()", () => {
 
 describe("deleteItem()", () => {
   it("should return the deleted item", async () => {
-    global.fetch = jest.fn(() =>
-      Promise.resolve({
-        json: () =>
-          Promise.resolve({
-            status: "success",
-            message: "it worked",
-            data: items[2],
-          }),
+    fetch.mockResponseOnce(
+      JSON.stringify({
+        status: "success",
+        message: "it worked",
+        data: items[2],
       })
     );
     const itemId = 3;
@@ -55,14 +44,11 @@ describe("deleteItem()", () => {
 
 describe("readItem()", () => {
   it("should return the item", async () => {
-    global.fetch = jest.fn(() =>
-      Promise.resolve({
-        json: () =>
-          Promise.resolve({
-            status: "success",
-            message: "it worked",
-            data: items[2],
-          }),
+    fetch.mockResponseOnce(
+      JSON.stringify({
+        status: "success",
+        message: "it worked",
+        data: items[2],
       })
     );
     const itemId = 3;
@@ -73,14 +59,11 @@ describe("readItem()", () => {
 
 describe("readItems()", () => {
   it("should return all items", async () => {
-    global.fetch = jest.fn(() =>
-      Promise.resolve({
-        json: () =>
-          Promise.resolve({
-            status: "success",
-            message: "it worked",
-            data: items,
-          }),
+    fetch.mockResponseOnce(
+      JSON.stringify({
+        status: "success",
+        message: "it worked",
+        data: items,
       })
     );
     const resp = await readItems();
@@ -91,14 +74,11 @@ describe("readItems()", () => {
 describe("updateItem()", () => {
   it("should return the modified items", async () => {
     const modifiedItem = { id: 2, name: "new name", brand: "Nike" };
-    global.fetch = jest.fn(() =>
-      Promise.resolve({
-        json: () =>
-          Promise.resolve({
-            status: "success",
-            message: "it worked",
-            data: modifiedItem,
-          }),
+    fetch.mockResponseOnce(
+      JSON.stringify({
+        status: "success",
+        message: "it worked",
+        data: modifiedItem,
       })
     );
     const resp = await updateItem(modifiedItem);
